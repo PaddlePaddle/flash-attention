17b89c9047017af7cb127bf5c1eec349e9045b5a
niuliling123 <sdust_niull@163.com>
2023-12-06 16:18:14 +0800

    updat

--- csrc/setup.py
+++ csrc/setup.py
@@ -8,8 +8,135 @@ from pathlib import Path
 from packaging.version import parse, Version
 import platform
 
+from setuptools import Command, Extension, setup
+from setuptools.command.develop import develop as DevelopCommandBase
+from setuptools.command.egg_info import egg_info
+from setuptools.command.install import install as InstallCommandBase
+from setuptools.command.install_lib import install_lib
+from setuptools.dist import Distribution
 from setuptools import setup, find_packages
 import subprocess
+python_version = platform.python_version()
+version_detail = sys.version_info
+version = version_detail[0] + version_detail[1] / 10
+env_version = os.getenv("PY_VERSION")
+
+if version < 3.7:
+    raise RuntimeError(
+        f"Paddle only supports Python version >= 3.7 now,"
+        f"you are using Python {python_version}"
+    )
+elif env_version is None:
+    print(f"export PY_VERSION = { python_version }")
+    os.environ["PY_VERSION"] = python_version
+
+elif env_version != version:
+    warnings.warn(
+        f"You set PY_VERSION={env_version}, but"
+        f"your current python environment is {version}"
+        f"we will use your current python version to execute"
+    )
+    os.environ["PY_VERSION"] = python_version
+
+
+global env_dict  # noqa: F811
+env_dict={
+    'PADDLE_SOURCE_DIR':'@PADDLE_SOURCE_DIR@',
+    'PADDLE_VERSION':'@PADDLE_VERSION@',
+    'PADDLE_BINARY_DIR':'@PADDLE_BINARY_DIR@',
+    'TAG_VERSION_REGEX':'@TAG_VERSION_REGEX@',
+    'WITH_GPU':'@WITH_GPU@',
+    'CUDNN_MAJOR_VERSION':'@CUDNN_MAJOR_VERSION@',
+    'CUDNN_MINOR_VERSION':'@CUDNN_MINOR_VERSION@',
+    'CUDNN_PATCHLEVEL_VERSION':'@CUDNN_PATCHLEVEL_VERSION@',
+    'CUDA_VERSION':'@CUDA_VERSION@',
+    'WITH_PSLI':'@WITH_PSLI@',
+    'FLUID_CORE_NAME':'@FLUID_CORE_NAME@',
+    'PHI_LIB':'@PHI_LIB@',
+    'PHI_NAME':'@PHI_NAME@',
+    'WITH_SHARED_PHI':'@WITH_SHARED_PHI@',
+    'IR_LIB':'@IR_LIB@',
+    'IR_NAME':'@IR_NAME@',
+    'WITH_SHARED_IR':'@WITH_SHARED_IR@',
+    'COMMON_LIB':'@COMMON_LIB@',
+    'COMMON_NAME':'@COMMON_NAME@',
+    'WARPCTC_LIBRARIES':'@WARPCTC_LIBRARIES@',
+    'WARPRNNT_LIBRARIES':'@WARPRNNT_LIBRARIES@',
+    'FLASHATTN_LIBRARIES':'@FLASHATTN_LIBRARIES@',
+    'LAPACK_LIB':'@LAPACK_LIB@',
+    'GFORTRAN_LIB':'@GFORTRAN_LIB@',
+    'GNU_RT_LIB_1':'@GNU_RT_LIB_1@',
+    'WITH_CUDNN_DSO':'@WITH_CUDNN_DSO@',
+    'CUDNN_LIBRARY':'@CUDNN_LIBRARY@',
+    'GNU_RT_LIB_2':'@GNU_RT_LIB_2@',
+    'WITH_MKL':'@WITH_MKL@',
+    'MKLML_SHARED_LIB':'@MKLML_SHARED_LIB@',
+    'MKLML_SHARED_IOMP_LIB':'@MKLML_SHARED_IOMP_LIB@',
+    'OPENBLAS_SHARED_LIB':'@OPENBLAS_SHARED_LIB@',
+    'OPENBLAS_LIB':'@OPENBLAS_LIB@',
+    'BLAS_LIB':'@BLAS_LIB@',
+    'WITH_LITE':'@WITH_LITE@',
+    'LITE_SHARED_LIB':'@LITE_SHARED_LIB@',
+    'LITE_WITH_NNADAPTER':'@LITE_WITH_NNADAPTER@',
+    'LITE_NNADAPTER_LIB':'@LITE_NNADAPTER_LIB@',
+    'NNADAPTER_WITH_HUAWEI_ASCEND_NPU':'@NNADAPTER_WITH_HUAWEI_ASCEND_NPU@',
+    'LITE_NNADAPTER_NPU_LIB':'@LITE_NNADAPTER_NPU_LIB@',
+    'WITH_CINN':'@WITH_CINN@',
+    'CINN_LIB_LOCATION':'@CINN_LIB_LOCATION@',
+    'CINN_LIB_NAME':'@CINN_LIB_NAME@',
+    'CINN_INCLUDE_DIR':'@CINN_INCLUDE_DIR@',
+    'CMAKE_BUILD_TYPE':'@CMAKE_BUILD_TYPE@',
+    'PSLIB_LIB':'@PSLIB_LIB@',
+    'JVM_LIB':'@JVM_LIB@',
+    'PSLIB_VERSION_PY':'@PSLIB_VERSION_PY@',
+    'WITH_MKLDNN':'@WITH_MKLDNN@',
+    'MKLDNN_SHARED_LIB':'@MKLDNN_SHARED_LIB@',
+    'MKLDNN_INSTALL_DIR':'@MKLDNN_INSTALL_DIR@',
+    'WITH_ONNXRUNTIME':'@WITH_ONNXRUNTIME@',
+    'ONNXRUNTIME_SHARED_LIB':'@ONNXRUNTIME_SHARED_LIB@',
+    'PADDLE2ONNX_LIB':'@PADDLE2ONNX_LIB@',
+    'PADDLE2ONNX_LIB_NAME':'@PADDLE2ONNX_LIB_NAME@',
+    'ONNXRUNTIME_LIB_NAME':'@ONNXRUNTIME_LIB_NAME@',
+    'WITH_XPU':'@WITH_XPU@',
+    'XPU_API_LIB':'@XPU_API_LIB@',
+    'XPU_API_LIB_NAME':'@XPU_API_LIB_NAME@',
+    'XPU_RT_LIB':'@XPU_RT_LIB@',
+    'XPU_RT_LIB_NAME':'@XPU_RT_LIB_NAME@',
+    'WITH_XPU_BKCL':'@WITH_XPU_BKCL@',
+    'XPU_BKCL_LIB':'@XPU_BKCL_LIB@',
+    'XPU_BKCL_LIB_NAME':'@XPU_BKCL_LIB_NAME@',
+    'WITH_XPU_XFT':'@WITH_XPU_XFT@',
+    'XPU_XFT_LIB':'@XPU_XFT_LIB@',
+    'XPU_XFT_LIB_NAME':'@XPU_XFT_LIB_NAME@',
+    'THIRD_PARTY_PATH':'@THIRD_PARTY_PATH@',
+    'SETUP_LOG_FILE':'@SETUP_LOG_FILE@',
+    'WITH_STRIP':'@WITH_STRIP@',
+    'PACKAGE_NAME':'@PACKAGE_NAME@',
+    'PADDLE_VERSION':'@PADDLE_VERSION@',
+    'APPLE':'@APPLE@',
+    'externalError_INCLUDE_DIR':'@externalError_INCLUDE_DIR@',
+    'WITH_ROCM':'@WITH_ROCM@',
+    'ORIGIN':'@ORIGIN@',
+    'WIN32':'@WIN32@',
+    'JIT_RELEASE_WHL':'@JIT_RELEASE_WHL@',
+    'WITH_PSLIB':'@WITH_PSLIB@',
+    'PYBIND_INCLUDE_DIR':'@PYBIND_INCLUDE_DIR@',
+    'WITH_PYTHON':'@WITH_PYTHON@',
+    'WITH_CINN':'@WITH_CINN@',
+    'CINN_SOURCE_DIR':'@CINN_SOURCE_DIR@',
+    'WITH_CPP_DIST':'@WITH_CPP_DIST@',
+    'PADDLE_INSTALL_DIR':'@PADDLE_INSTALL_DIR@',
+    'PADDLE_LIB_TEST_DIR':'@PADDLE_LIB_TEST_DIR@'
+}
+
+global paddle_binary_dir, paddle_source_dir
+
+paddle_binary_dir = env_dict.get("PADDLE_BINARY_DIR")
+paddle_source_dir = env_dict.get("PADDLE_SOURCE_DIR")
+
+# preparing parameters for setup()
+paddle_version = env_dict.get("PADDLE_VERSION")
+package_name = env_dict.get("PACKAGE_NAME")
 
 import urllib.request
 import urllib.error
@@ -19,7 +146,7 @@ import paddle
 from paddle.utils.cpp_extension import BuildExtension, CppExtension, CUDAExtension
 from paddle.utils.cpp_extension.extension_utils import find_cuda_home
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("../README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
@@ -153,7 +280,7 @@ def get_data_files():
     source_lib_path = 'libflashattn.so'
 
     # Specify the destination directory within the package
-    destination_lib_path = os.path.join(PACKAGE_NAME, 'libflashattn.so')
+    destination_lib_path = os.path.join(PACKAGE_NAME, 'build/libflashattn.so')
 
     data_files.append((os.path.join(PACKAGE_NAME, 'libflashattn.so'), [source_lib_path]))
     return data_files
@@ -211,6 +338,145 @@ class CachedWheelsCommand(_bdist_wheel):
             # If the wheel could not be downloaded, build from source
             super().run()
 
+class InstallHeaders(Command):
+    """Override how headers are copied."""
+
+    description = 'install C/C++ header files'
+
+    user_options = [
+        ('install-dir=', 'd', 'directory to install header files to'),
+        ('force', 'f', 'force installation (overwrite existing files)'),
+    ]
+
+    boolean_options = ['force']
+
+    def initialize_options(self):
+        self.install_dir = None
+        self.force = 0
+        self.outfiles = []
+
+    def finalize_options(self):
+        self.set_undefined_options(
+            'install', ('install_headers', 'install_dir'), ('force', 'force')
+        )
+
+    def run(self):
+        hdrs = self.distribution.headers
+        if not hdrs:
+            return
+        self.mkpath(self.install_dir)
+        for header in hdrs:
+            install_dir = get_header_install_dir(header)
+            install_dir = os.path.join(
+                self.install_dir, os.path.dirname(install_dir)
+            )
+            if not os.path.exists(install_dir):
+                self.mkpath(install_dir)
+            (out, _) = self.copy_file(header, install_dir)
+            self.outfiles.append(out)
+            # (out, _) = self.mkdir_and_copy_file(header)
+            # self.outfiles.append(out)
+
+    def get_inputs(self):
+        return self.distribution.headers or []
+
+    def get_outputs(self):
+        return self.outfiles
+
+
+class InstallCommand(InstallCommandBase):
+    def finalize_options(self):
+        ret = InstallCommandBase.finalize_options(self)
+        self.install_lib = self.install_platlib
+
+        self.install_headers = os.path.join(
+            self.install_platlib, 'paddle', 'include'
+        )
+        return ret
+
+
+class DevelopCommand(DevelopCommandBase):
+    def run(self):
+        # copy proto and .so to python_source_dir
+        fluid_proto_binary_path = (
+            paddle_binary_dir + '/python/paddle/base/proto/'
+        )
+        fluid_proto_source_path = (
+            paddle_source_dir + '/python/paddle/base/proto/'
+        )
+        distributed_proto_binary_path = (
+            paddle_binary_dir + '/python/paddle/distributed/fleet/proto/'
+        )
+        distributed_proto_source_path = (
+            paddle_source_dir + '/python/paddle/distributed/fleet/proto/'
+        )
+        os.system(f"rm -rf {fluid_proto_source_path}")
+        shutil.copytree(fluid_proto_binary_path, fluid_proto_source_path)
+        os.system(f"rm -rf {distributed_proto_source_path}")
+        shutil.copytree(
+            distributed_proto_binary_path, distributed_proto_source_path
+        )
+        shutil.copy(
+            paddle_binary_dir + '/python/paddle/base/libpaddle.so',
+            paddle_source_dir + '/python/paddle/base/',
+        )
+        dynamic_library_binary_path = paddle_binary_dir + '/python/paddle/libs/'
+        dynamic_library_source_path = paddle_source_dir + '/python/paddle/libs/'
+        for lib_so in os.listdir(dynamic_library_binary_path):
+            shutil.copy(
+                dynamic_library_binary_path + lib_so,
+                dynamic_library_source_path,
+            )
+        # write version.py and cuda_env_config_py to python_source_dir
+        write_version_py(
+            filename=f'{paddle_source_dir}/python/paddle/version/__init__.py'
+        )
+        write_cuda_env_config_py(
+            filename=f'{paddle_source_dir}/python/paddle/cuda_env.py'
+        )
+        write_parameter_server_version_py(
+            filename='{}/python/paddle/incubate/distributed/fleet/parameter_server/version.py'.format(
+                paddle_source_dir
+            )
+        )
+        DevelopCommandBase.run(self)
+
+
+class EggInfo(egg_info):
+    """Copy license file into `.dist-info` folder."""
+
+    def run(self):
+        # don't duplicate license into `.dist-info` when building a distribution
+        if not self.distribution.have_run.get('install', True):
+            self.mkpath(self.egg_info)
+            #self.copy_file(
+            #    env_dict.get("PADDLE_SOURCE_DIR") + "/LICENSE", self.egg_info
+            #)
+
+        egg_info.run(self)
+
+
+# class Installlib is rewritten to add header files to .egg/paddle
+class InstallLib(install_lib):
+    def run(self):
+        self.build()
+        outfiles = self.install()
+        hrds = self.distribution.headers
+        if not hrds:
+            return
+        for header in hrds:
+            install_dir = get_header_install_dir(header)
+            install_dir = os.path.join(
+                self.install_dir, 'paddle/include', os.path.dirname(install_dir)
+            )
+            if not os.path.exists(install_dir):
+                self.mkpath(install_dir)
+            self.copy_file(header, install_dir)
+        if outfiles is not None:
+            # always compile, in case we have any extension stubs to deal with
+            self.byte_compile(outfiles)
+
+
 
 setup(
     name=PACKAGE_NAME,
@@ -227,17 +493,24 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/PaddlePaddle/flash-attention",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 37",
         "License :: OSI Approved :: BSD License",
         "Operating System :: Unix",
     ],
     ext_modules=ext_modules,
     cmdclass={
-        'bdist_wheel': CachedWheelsCommand,
-        "build_ext": BuildExtension
-    } if ext_modules else {
-        'bdist_wheel': CachedWheelsCommand,
+        'install_headers': InstallHeaders,
+        'install': InstallCommand,
+        'egg_info': EggInfo,
+        'install_lib': InstallLib,
+        'develop': DevelopCommand,
     },
+    #cmdclass={
+    #    "bdist_wheel": CachedWheelsCommand,
+    #    "build_ext": BuildExtension
+    #} if ext_modules else {
+    #    "bdist_wheel": CachedWheelsCommand,
+    #},
     python_requires=">=3.7",
     install_requires=[
         "paddle",
